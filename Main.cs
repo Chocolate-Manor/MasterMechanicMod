186bbdbffbd0188274db99f1062cd8d1931d6492
Haocheng Yang <alexyanghaocheng@gmail.com>
2024-04-12 21:25:03 +0100

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityModManagerNet;
+using HarmonyLib;
+using System.Reflection;
+
+namespace MasterMechanicMod
+{
+    public class Main
+    {
+        public static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnToggle = new Func<UnityModManager.ModEntry, bool, bool>(Main.OnToggle);
+
+            Harmony harmony = new Harmony(modEntry.Info.Id);
+
+            // patching the current assembly
+            var curAssembly = Assembly.GetExecutingAssembly();
+            harmony.PatchAll(curAssembly);
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            if(value == true)
+            {
+                modEntry.Logger.Log("Master Mechanic Mod On");
+            }
+            else
+            {
+                modEntry.Logger.Log("Master Mechanic Mod Off");
+            }
+
+            enabled = value;
+            return true;
+        }
+    }
+}
---
+++ Patches/ShadersBackupPatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace MasterMechanicMod.Patches
+{
+    [HarmonyPatch(typeof(ShadersBackup), nameof(ShadersBackup.Backup))]
+    class ShadersBackupPatch
+    {
+        static void Postfix(GameObject go)
+        {
+            if (!Main.enabled) return;
+
+            Renderer component = go.GetComponent<Renderer>();
+            if (component == null)
+            {
+                return;
+            }
+            component.enabled = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(ShadersBackup), nameof(ShadersBackup.RevertToBackup))]
+    class ShadersRevertToBackupPatch
+    {
+        static void Postfix(GameObject go)
+        {
+            if (!Main.enabled) return;
+
+            Renderer component = go.GetComponent<Renderer>();
+            if (component == null)
+            {
+                return;
+            }
+            component.enabled = true;
+        }
+    }
+}
